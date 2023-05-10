3338748c4f237fd24ff3c8920145ead4f9797b1a
[Macarena Mouro] <macamouro@gmail.com>
2023-05-10 15:16:06 -0300

    add styles mobile first

--- src/modules/productList/layouts/ProductListLayout.vue
+++ src/modules/productList/layouts/ProductListLayout.vue
@@ -1,84 +1,75 @@
 <template>
-    <NavbarItem />
+  <NavbarItem />
 
-    <div v-if="isLoading"
-        class="row justify-content-md-center">
-        <div class="col-3 alert-info text-center mt-5">
-            Espere por favor...
-            <h3 class="mt-2">
-                <i class="fa fa-spin fa-sync"></i>
-            </h3>
-        </div>
+  <div v-if="isLoading" class="row justify-content-md-center">
+    <div class="col-3 alert-info text-center mt-5">
+      Espere por favor...
+      <h3 class="mt-2">
+        <i class="fa fa-spin fa-sync"></i>
+      </h3>
     </div>
-    
-    <div v-else 
-        class="d-flex">
-        <div class="col-3">
-            <EntryList />
-        </div>
-        <div class="col">
-            <router-view />
-            <hr>
-            <div class="total-list-wrapper">
-                <TotalList />
-            </div>
-        </div>
-    </div>
+  </div>
 
+  <div v-else class="columns-wrapper">
+    <div>
+      <EntryList />
+    </div>
+    <div>
+      <router-view />
+      <hr />
+      <div class="total-list-wrapper">
+        <TotalList />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
-import { defineAsyncComponent } from 'vue'
-import { mapActions, mapState } from 'vuex'
+import { defineAsyncComponent } from "vue";
+import { mapActions, mapState } from "vuex";
 
 export default {
-
-    components: {
-        NavbarItem: defineAsyncComponent( () => import('../components/NavbarItem.vue')),
-        EntryList: defineAsyncComponent( () => import('../components/EntryList.vue')),
-        TotalList: defineAsyncComponent( () => import('../components/TotalList.vue')),
-    },
-    data() {
-        return {
-            isLoaded: false // Bandera para controlar si se ha cargado la lista de entradas
-        }
-    },
-    methods: {
-        ...mapActions('productListModule', ['loadEntries'])
-    },
-    computed: {
-        ...mapState('productListModule', ['isLoading'])
-    },
-    created() {
-        // Si la lista de entradas aún no se ha cargado, llame a la acción loadEntries
-        if (!this.isLoaded) {
-            this.loadEntries()
-            this.isLoaded = true
-        }
-    },
-    beforeRouteEnter(to, from, next) {
-        // Verifique si el componente ya se ha creado y la lista de entradas se ha cargado
-        if (from.name === null) {
-            next(vm => {
-                if (!vm.isLoaded) {
-                    vm.loadEntries()
-                    vm.isLoaded = true
-                }
-            })
-        } else {
-            next()
-        }
+  components: {
+    NavbarItem: defineAsyncComponent(() =>
+      import("../components/NavbarItem.vue")
+    ),
+    EntryList: defineAsyncComponent(() =>
+      import("../components/EntryList.vue")
+    ),
+    TotalList: defineAsyncComponent(() =>
+      import("../components/TotalList.vue")
+    ),
+  },
+  data() {
+    return {
+      isLoaded: false, // Bandera para controlar si se ha cargado la lista de entradas
+    };
+  },
+  methods: {
+    ...mapActions("productListModule", ["loadEntries"]),
+  },
+  computed: {
+    ...mapState("productListModule", ["isLoading"]),
+  },
+  created() {
+    // Si la lista de entradas aún no se ha cargado, llame a la acción loadEntries
+    if (!this.isLoaded) {
+      this.loadEntries();
+      this.isLoaded = true;
     }
-    
-}
+  },
+  beforeRouteEnter(to, from, next) {
+    // Verifique si el componente ya se ha creado y la lista de entradas se ha cargado
+    if (from.name === null) {
+      next((vm) => {
+        if (!vm.isLoaded) {
+          vm.loadEntries();
+          vm.isLoaded = true;
+        }
+      });
+    } else {
+      next();
+    }
+  },
+};
 </script>
-
-<style scoped>
-
-.d-flex {
-    position: relative;
-    gap: 10px;
-}
-
-</style>
--- src/modules/productList/views/EntryView.vue
+++ src/modules/productList/views/EntryView.vue
@@ -7,7 +7,8 @@
           v-model="entry.name"
           placeholder="Nombre*"
           @input="validateName"
-          :class="{ invalid: !isNameValid }" />
+          :class="{ invalid: !isNameValid }"
+        />
         <label :class="{ invalid: !isNameValid }">{{ nameLabel }}</label>
       </div>
       <div class="input-wrapper">
@@ -16,7 +17,8 @@
           v-model="entry.price"
           placeholder="Precio*"
           @input="validatePrice"
-          :class="{ invalid: !isPriceValid }" />
+          :class="{ invalid: !isPriceValid }"
+        />
         <label :class="{ invalid: !isPriceValid }">{{ priceLabel }}</label>
       </div>
       <div class="input-wrapper">
@@ -25,31 +27,38 @@
           v-model="entry.quantity"
           placeholder="Cantidad*"
           @input="validateQuantity"
-          :class="{ invalid: !isQuantityValid }" />
-        <label :class="{ invalid: !isQuantityValid }">{{ quantityLabel }}</label>
+          :class="{ invalid: !isQuantityValid }"
+        />
+        <label :class="{ invalid: !isQuantityValid }">{{
+          quantityLabel
+        }}</label>
       </div>
 
       <div class="buttons-wrapper">
-        <button class="btn btn-save mx-2" @click="saveEntry"><i class="fa fa-save alt"></i></button>
+        <button class="btn btn-save mx-2" @click="saveEntry">
+          <i class="fa fa-save alt"></i>
+        </button>
         <button
           v-if="entry.id"
           class="btn btn-trash mx-2"
-          @click="onDeleteEntry"><i class="fa fa-trash alt"></i></button>
+          @click="onDeleteEntry"
+        >
+          <i class="fa fa-trash alt"></i>
+        </button>
       </div>
-  </div>
+    </div>
   </template>
 </template>
 
 <script>
-
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   props: {
     id: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
 
   data() {
@@ -58,72 +67,75 @@ export default {
       isNameValid: true,
       isPriceValid: true,
       isQuantityValid: true,
-    }
+    };
   },
 
   computed: {
-    ...mapGetters('productListModule', ['getEntriesById']),
+    ...mapGetters("productListModule", ["getEntriesById"]),
     nameLabel() {
-      return this.isNameValid ? 'Ingresá el producto' : 'Nombre incorrecto';
+      return this.isNameValid ? "Ingresá el producto" : "Nombre incorrecto";
     },
     priceLabel() {
-      return this.isPriceValid ? 'Ingresá el precio' : 'Precio incorrecto';
+      return this.isPriceValid ? "Ingresá el precio" : "Precio incorrecto";
     },
     quantityLabel() {
-      return this.isQuantityValid ? 'Ingresá la cantidad' : 'Cantidad incorrecta';
+      return this.isQuantityValid
+        ? "Ingresá la cantidad"
+        : "Cantidad incorrecta";
     },
     isInvalid() {
       return !this.isNameValid || !this.isPriceValid || !this.isQuantityValid;
-    }
+    },
   },
 
   methods: {
-    ...mapActions('productListModule', ['updateEntry', 'createEntry', 'deleteEntry']),
+    ...mapActions("productListModule", [
+      "updateEntry",
+      "createEntry",
+      "deleteEntry",
+    ]),
     loadEntry() {
-
       let entry;
 
-      if(this.id === 'new') {
+      if (this.id === "new") {
         entry = {
-          name: ''
-        }
+          name: "",
+        };
       } else {
-
-        entry = this.getEntriesById( this.id ) 
-
+        entry = this.getEntriesById(this.id);
       }
 
-      this.entry = entry
+      this.entry = entry;
     },
     async saveEntry() {
-
       if (this.isInvalid) {
         this.validateName();
         this.validatePrice();
         this.validateQuantity();
         return;
       }
-      
-      if (this.entry.id) {
-        await this.updateEntry( this.entry )
-      } else {
-        await this.createEntry( this.entry )
-      }
 
+      if (this.entry.id) {
+        await this.updateEntry(this.entry);
+      } else {
+        await this.createEntry(this.entry);
+      }
     },
     async onDeleteEntry() {
-      console.log('delete', this.entry)
-      await this.deleteEntry( this.entry.id )
+      console.log("delete", this.entry);
+      await this.deleteEntry(this.entry.id);
     },
     validateName() {
-      this.isNameValid = /^[a-zA-Z]+(?: [a-zA-Z]+)*(\s\d+[a-zA-Z]*)?$/.test(this.entry.name);
+      this.isNameValid = /^[a-zA-Z]+(?: [a-zA-Z]+)*(\s\d+[a-zA-Z]*)?$/.test(
+        this.entry.name
+      );
     },
     validatePrice() {
       if (this.entry.price <= 0 || isNaN(this.entry.price)) {
         this.isPriceValid = false;
       } else {
         this.isPriceValid = true;
-        this.entry.price = Number(this.entry.price.toFixed(2))
+        this.entry.price = Number(this.entry.price.toFixed(2));
       }
     },
     validateQuantity() {
@@ -135,19 +147,26 @@ export default {
     },
   },
   created() {
-    this.loadEntry()
+    this.loadEntry();
   },
   watch: {
     id() {
-      this.loadEntry()
-    }
-  }
-}
+      this.loadEntry();
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 /* el form wrapper es el que hay que ponerle un media querie */
+
+.form-wrapper {
+  gap: 7px;
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
 .input-wrapper {
   display: flex;
@@ -183,8 +202,15 @@ label.invalid {
 
 .btn {
   width: 40px;
-  border: 1px solid;
+  background-color: #590696;
+  color: #37e2d5;
+  width: 40px;
+  border: 1px none;
   border-radius: 50%;
+  transition: transfortm 5s ease-in-out;
 }
 
+.btn:active {
+  transform: scale(3);
+}
 </style>
